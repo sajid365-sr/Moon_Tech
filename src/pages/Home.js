6b0c89e2874edb0e5d08024b74520521c59183d0
sajid365-sr <sajidsorker86@gmail.com>
2022-12-17 03:37:32 +0600

    use reducer setup, context api setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { useProduct } from "../context/ProductProvider";
 
 const Home = () => {
 
+const initialState = {};
+
+const productReducer = (state, action) =>{
+
+
+}
+
+  const [state, dispatch] = useReducer(productReducer,initialState);
+  
   useEffect( () =>{
-    fetch()
+    fetch('http://localhost:5000/products')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
   },[]);
+  
   const { test } = useProduct();
-  console.log(test);
+
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
